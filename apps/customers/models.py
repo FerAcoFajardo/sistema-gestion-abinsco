ee7d01b5d80763f29531178b2d57b3355a8bc87e
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-03-10 01:18:13 -0700

    change in phone max length

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -5,7 +5,7 @@ class Customers(models.Model):
     name = models.CharField(max_length=100, null= False, blank=False)
     rfc = models.CharField(max_length=13, null=False, blank=False)
     address = models.CharField(max_length=200, null=False, blank=True)
-    phone = models.CharField(max_length=10, null=False, blank=True)
+    phone = models.CharField(max_length=18, null=False, blank=True)
     email = models.EmailField(null=False, blank=True)
     # credit = models.ForeignKey('credit.Credit', on_delete=models.CASCADE)
     # sale = models.ForeignKey('sales.Sales', on_delete=models.CASCADE)
--- apps/providers/models.py
+++ apps/providers/models.py
@@ -6,7 +6,7 @@ class Providers (models.Model):
     name= models.CharField(max_length=100, null=False, blank=False)
     rfc = models.CharField(max_length=13, null= False, blank = False)
     address= models.CharField(max_length=200, null=False, blank=True)
-    phone = models.CharField(max_length=10, null=False, blank=True)
+    phone = models.CharField(max_length=18, null=False, blank=True)
     email = models.CharField(max_length=20, null=False, blank=True)
 
     class Meta:
