9e292c1e44bb758899f897f68ec83b54c3999e8f
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-03-21 18:12:33 -0700

    added normal sale with credit test

--- apps/sales/tests.py
+++ apps/sales/tests.py
@@ -4,24 +4,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.support.select import Select
 
-# Create your tests here.
-
 class SalesTestCase(TestCase):
-
     
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window() # For maximizing window
+        driver = cls.driver
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
-        #super(test_sales, cls).tearDownClass()
-    
-    def setUp(self):
-        driver = self.driver
-
-        #driver.get('http://127.0.0.1:8000/')
         driver.get('http://localhost:8000/')
 
         username = driver.find_element_by_name('username')
@@ -36,43 +26,68 @@ class SalesTestCase(TestCase):
 
         assert 'admin' in driver.page_source
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        #super(test_sales, cls).tearDownClass()
+    
     def test_normal_sale_without_credit(self):
         driver = self.driver
 
         driver.get('http://localhost:8000/sales/create')
-        
+
+        select_customer = Select(driver.find_element_by_id('id_customer'))
+        select_customer.select_by_index('1')
 
         select_product = Select(driver.find_element_by_id('id_form-__prefix__-product'))
+        select_product.select_by_index('1')
 
         btn_add_product = driver.find_element_by_id('add-form')
-
-        select_product.select_by_value('1')
-
         btn_add_product.send_keys(Keys.RETURN)
-
-
         select_product.select_by_value('2')
-
         btn_add_product.send_keys(Keys.RETURN)
 
         quantity_first_product = driver.find_element_by_id('cart-index-0')
-
         quantity_first_product.send_keys(Keys.ARROW_UP)
         quantity_first_product.send_keys(Keys.ARROW_UP)
 
         submit = driver.find_element_by_id('submit-btn')
-
         submit.send_keys(Keys.RETURN)
 
-        time.sleep(5)
+        self.assertEqual("Tutorialspoint", "Tutorialspoint")
+        #assert "Ventas" in driver.title
 
+        # para dar click al select2
         #customer = driver.find_element_by_css_selector("#s2id_customer .select2-choice")
         #customer = driver.find_element_by_xpath("//span[@class='selection']")
         #customer.click()
+    
 
-        #time.sleep(5)
+    def test_normal_sale_with_credit(self):
 
-        #customer.select_by_value('1')
+        driver = self.driver
 
+        driver.get('http://localhost:8000/sales/create')
+        
+        select_customer = Select(driver.find_element_by_id('id_customer'))
+        select_customer.select_by_index('1')
 
-        #assert "Ventas" in driver.title
+        radio_credit = driver.find_element_by_id('credito')
+        radio_credit.click()
+        
+        select_product = Select(driver.find_element_by_id('id_form-__prefix__-product'))
+        select_product.select_by_index('1')
+
+        btn_add_product = driver.find_element_by_id('add-form')
+        btn_add_product.send_keys(Keys.RETURN)
+        select_product.select_by_value('2')
+        btn_add_product.send_keys(Keys.RETURN)
+
+        quantity_first_product = driver.find_element_by_id('cart-index-0')
+        quantity_first_product.send_keys(Keys.ARROW_UP)
+        quantity_first_product.send_keys(Keys.ARROW_UP)
+
+        submit = driver.find_element_by_id('submit-btn')
+        #submit.send_keys(Keys.RETURN)
+        
+        self.assertEqual("Tutorialspoint", "Tutorialspoint")
