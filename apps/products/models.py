ea7dd90d6ea951a48ae06f4aeb4fb10d015978f7
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-05-11 21:18:30 -0700

    Change in categories str

--- apps/products/models.py
+++ apps/products/models.py
@@ -40,6 +40,9 @@ class PositiveFloatField(models.FloatField):
 
 class Categories (models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
