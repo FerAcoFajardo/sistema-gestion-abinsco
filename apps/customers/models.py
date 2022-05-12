be98b39dbc9f91778559e109ae287e6cf01a2672
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-05-11 21:09:19 -0700

    Change in models to validate things

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+
+def validate_positive(value):
+    if value < 0:
+        raise ValidationError(
+            _("%(value)s is not positive."),
+            params={"value": value}
+        )
+
+
+class PositiveDecimalField(models.DecimalField):
+    description = _("Número decimal positivo")
+
+    @cached_property
+    def validators(self):
+        return super().validators + [validate_positive]
 
 
 class Customers(models.Model):
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+
 from random import randrange
 
 # Create your models here.
@@ -17,14 +21,31 @@ UNITY_CHOICES = [
     ('kg', 'KG'),
 ]
 
+
+def validate_positive(value):
+    if value < 0:
+        raise ValidationError(
+            _("%(value)s is not positive."),
+            params={"value": value}
+        )
+
+
+class PositiveFloatField(models.FloatField):
+    description = _("Número decimal positivo")
+
+    @cached_property
+    def validators(self):
+        return super().validators + [validate_positive]
+
+
 class Categories (models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
 
 class Products(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    current_price = models.FloatField(null=False, blank=False)
-    in_storage = models.IntegerField(null=False, blank=False)
+    current_price = PositiveFloatField(null=False, blank=False)
+    in_storage = models.PositiveIntegerField(null=False, blank=False)
     code = models.CharField(max_length=10, null=False, blank=False)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     unity = models.CharField(max_length=20,choices=UNITY_CHOICES, default='pz' ,null=False, blank=False) # Enum
